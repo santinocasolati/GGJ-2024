f86e9f8bbde0cfdf7a06907a32fbd344b228b0a8
Santino Casolati <63636985+santinocasolati@users.noreply.github.com>
2024-01-24 20:05:41 -0300

    Defeat Screen Added

---
+++ Assets/Scripts/UI/FadeBlack.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeBlack : MonoBehaviour
+{
+    public float fadeSpeed;
+
+    private Image image;
+
+    private void Start()
+    {
+        image = GetComponent<Image>();
+        StartCoroutine(FadeOut());
+    }
+
+    IEnumerator FadeOut()
+    {
+        Color currentColor = image.color;
+
+        while (currentColor.a > 0)
+        {
+            currentColor.a -= fadeSpeed * Time.deltaTime;
+            image.color = currentColor;
+            yield return null;
+        }
+
+        Destroy(gameObject);
+    }
+}
