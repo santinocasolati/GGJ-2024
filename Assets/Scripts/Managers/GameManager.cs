f0291035866ad6b8ee9a39d9954f56f1cc5a74c0
Santino Casolati <63636985+santinocasolati@users.noreply.github.com>
2024-01-27 11:52:09 -0300

    Camera Transitions Added

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject[] cameras;
+    public bool[] testCameras;
+
+    public void ChangeArea(int area)
+    {
+        cameras[area].SetActive(true);
+
+        for (int i = 0; i < cameras.Length; i++)
+        {
+            if (i == area) continue;
+
+            cameras[i].SetActive(false);
+        }
+    }
+
+    private void Update()
+    {
+#if UNITY_EDITOR
+        for (int i = 0; i < testCameras.Length; i++)
+        {
+            if (testCameras[i])
+            {
+                testCameras[i] = false;
+                ChangeArea(i);
+            }
+        }
+#endif
+    }
+}
