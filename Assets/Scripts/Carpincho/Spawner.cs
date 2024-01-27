39049ea1cbbe25e1b01792f206bfff20f1431e47
MauroUr <mauro.uriarte00@gmail.com>
2024-01-27 12:29:31 -0300

    Spawner funcional, con comentarios para cuando haya mas prefabs de enemigos

--- Assets/Scripts/Carpincho/Spawner.cs
+++ Assets/Scripts/Carpincho/Spawner.cs
@@ -14,7 +14,7 @@ public class Spawner : MonoBehaviour
     private int torretaCounter = 3;
     private int paracaidasCounter = 1;
     private const float spawnInterval = 5f;
-    private float timeSinceNoEnemies = 0.0f;
+    private float timeSinceNoEnemies = 6f;
     private int waveNumber = 0;
 
     bool waveCleared = true;
@@ -51,25 +51,25 @@ public class Spawner : MonoBehaviour
             waveNumber++;
             //waveText.text = $"Wave {waveNumber}/4";
             waveNumber = 0;
-        }
-        if (GameObject.FindGameObjectWithTag("Enemy") == null)
-        {
-            timeSinceNoEnemies += Time.deltaTime;
-            if (!waveCleared && waveNumber == 4) {
-                waveCleared = true;
-                torretaCounter --;
-                paracaidasCounter --;
-            }
         }*/
+        if (GameObject.FindGameObjectWithTag("Enemy") == null)
+            timeSinceNoEnemies += Time.deltaTime;
+
     }
 
     void SpawnEnemy(GameObject enemyPrefab)
     {
-        Vector3 spawnPos = this.player.transform.position;
-        while (Vector3.Distance(spawnPos, player.transform.position) < 5f)
-        {
-            spawnPos = this.transform.position + new Vector3(Random.Range(-this.transform.localScale.x / 2, this.transform.localScale.x / 2), Random.Range(-this.transform.localScale.y / 3, this.transform.localScale.y / 3), Random.Range(-this.transform.localScale.z / 2, this.transform.localScale.z / 2));
-        }
+        Vector3 playerPos = this.player.transform.position;
+        Vector3 spawnPos = playerPos;
+        
+
+        if (Random.Range(0, 2) == 0)
+            spawnPos.x += Camera.main.pixelWidth / 45;
+        else
+            spawnPos.x -= Camera.main.pixelWidth / 45;
+
         Instantiate(enemyPrefab, spawnPos, Quaternion.identity);
     }
+
+
 }
