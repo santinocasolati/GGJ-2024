905b85c9edaf87415a907dc7427e726c92f3beae
nimbl3_ <tekiadp@gmail.com>
2024-01-28 07:39:20 -0300

    Particle System + Particle Shaders

--- Assets/Scripts/Carpincho/Parachute.cs
+++ Assets/Scripts/Carpincho/Parachute.cs
@@ -4,34 +4,58 @@ using UnityEngine;
 
 public class Parachute : Enemy
 {
-    private Animator animator;
+    public GameObject bomber;
+    public float power = 10.0f;
+    public float radius = 5.0f;
+    public float upForce = 1.0f;
 
+    [SerializeField] public GameObject particleEffect;
 
     private void Awake()
     {
-        animator = transform.GetChild(0).GetComponent<Animator>();
+
     }
 
-    private void Update()
-    {
-        if (player != null && !this.isStunned)
-        {
+    /* private void Update()
+     {
+         if (player != null && !this.isStunned)
+         {
+             transform.position = Vector3.MoveTowards(transform.position, );
+         }
+     }*/
 
+    private void FixedUpdate()
+    {
+        if (bomber == enabled)
+        {
+            //Invoke("Attack", 5);
         }
     }
 
+    private void Attack()
+    {
+        Vector3 explosionPos = bomber.transform.position;
+        Collider[] colliders = Physics.OverlapSphere(explosionPos, radius);
+        
 
+        foreach (Collider hit in colliders)
+        {
+            Rigidbody rb = hit.GetComponent<Rigidbody>();
+            if (rb != null)
+            {
+                rb.AddExplosionForce(power, explosionPos, radius, upForce, ForceMode.Impulse);
+            }
+        }
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-
+    private void OnTriggerEnter(Collider other)
+    { 
+        if (other.gameObject.tag == "Floor")
+        {
+            //Attack();
+            Instantiate(particleEffect, transform.position, transform.rotation);
+            Destroy(gameObject);
+            
+        }
+    }
 }
